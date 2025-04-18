4646a7639498fb206f9ca4b293d5fff6925aea13
Wmunga <mungawonder11@gmail.com>
2025-04-18 23:20:41 +0300

    my group commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from pet import Pet
+
+# Create a pet instance
+my_pet = Pet("Buddy")
+
+# Test pet actions
+my_pet.get_status()
+my_pet.eat()
+my_pet.play()
+my_pet.sleep()
+my_pet.train("roll over")
+my_pet.train("fetch")
+my_pet.get_status()
+my_pet.show_tricks()
